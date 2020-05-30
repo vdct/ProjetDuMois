eb4fcad903073c48682e5eb7912c8064147a108f
PanierAvide <panieravide@riseup.net>
2020-05-30 12:17:01 +0200

    Show Osmose items on map

--- website/index.js
+++ website/index.js
@@ -7,9 +7,25 @@ const cors = require('cors');
 const compression = require('compression');
 const path = require('path');
 const projects = require('./projects');
-const pastProjects = Object.entries(projects)
-	.filter(p => typeof p[1] === "object" && p[1].id !== projects.current)
-	.map(p => ({ id: p[1].id, label: `${new Date(p[1].start_date).toLocaleDateString('fr-FR', { year: 'numeric', month: 'numeric' })} - ${p[1].title}` }));
+const CONFIG = require('../config.json');
+
+// Get current+past projects
+function filterProjects() {
+	const prjs = { past: [], current: null };
+	Object.values(projects).forEach(project => {
+		// Check dates
+		if(new Date(project.start_date).getTime() <= Date.now() && Date.now() <= new Date(project.end_date).getTime()) {
+			prjs.current = project;
+		}
+		else {
+			prjs.past.push({
+				id: project.id,
+				label: `${new Date(project.start_date).toLocaleDateString('fr-FR', { year: 'numeric', month: 'numeric' })} - ${project.title}`
+			});
+		}
+	});
+	return prjs;
+}
 
 
 /*
@@ -26,7 +42,66 @@ app.set('views', __dirname+'/templates');
 
 // Index
 app.get('/', (req, res) => {
-	res.render('index', Object.assign({ past: pastProjects }, projects[projects.current]));
+	const p = filterProjects();
+	res.render('index', Object.assign({ past: p.past }, p.current));
+});
+
+// Mapbox GL style
+app.get('/mapstyle.json', (req, res) => {
+	const osmoseSources = {};
+	const osmoseLayers = [];
+	const p = filterProjects();
+
+	if(p.current) {
+		p.current.datasources
+		.filter(ds => ds.source === "osmose")
+		.forEach(ds => {
+			const id = `osmose_${ds.item}_${ds.class}`;
+
+			osmoseSources[id] = {
+				type: "vector",
+				tiles: [ `${CONFIG.OSMOSE_URL}/api/0.3beta/issues/{z}/{x}/{y}.mvt?item=${ds.item}${ds.class ? "&class="+ds.class : ""}` ],
+				minzoom: 7
+			};
+
+			osmoseLayers.push({
+				id: id,
+				source: id,
+				type: "circle",
+				"source-layer": "issues",
+				paint: {
+					"circle-color": ds.color || "red",
+					"circle-radius": [
+						"interpolate",
+						["linear"],
+						["zoom"],
+						11, 3,
+						19, 13
+					]
+				}
+			});
+		});
+	}
+
+	res.json({
+		version: 8,
+		name: "ProjetDuMois.fr",
+		sources: Object.assign({
+			osm: {
+				type: "raster",
+				tiles: [ "https://tile.openstreetmap.org/{z}/{x}/{y}.png" ],
+				maxzoom: 19,
+				attribution: "&copy; OpenStreetMap"
+			}
+		}, osmoseSources),
+		layers: [
+			{
+				id: "osm",
+				source: "osm",
+				type: "raster"
+			}
+		].concat(osmoseLayers)
+	});
 });
 
 // Documentation
--- website/projects.js
+++ website/projects.js
@@ -6,11 +6,6 @@ fs.readdirSync(PROJECTS_PATH).forEach(projectDir => {
 	try {
 		const project = JSON.parse(fs.readFileSync(PROJECTS_PATH + '/' + projectDir + '/info.json'));
 		projects[project.id] = project;
-
-		// Check dates
-		if(new Date(project.start_date).getTime() <= Date.now() && Date.now() <= new Date(project.end_date).getTime()) {
-			projects.current = project.id;
-		}
 	}
 	catch(e) {
 		console.error("Invalid project", projectDir, e);

d9770cef40ab263c453a35c2606704e9b5196cdd
PanierAvide <panieravide@riseup.net>
2020-08-17 14:56:47 +0200

    Minimal OSM editor (notes + features)

--- website/utils.js
+++ website/utils.js
@@ -63,6 +63,31 @@ exports.getMapStyle = (p) => {
 			]
 		};
 
+		// OSM
+		p.datasources
+		.filter(ds => ds.source === "osm")
+		.forEach((ds, dsid) => {
+			const id = `osm_${dsid}`;
+			const color = ds.color || "#2E7D32";
+			const layer = `public.project_${p.id.split("_").pop()}`;
+			sources[id] = {
+				type: "vector",
+				tiles: [ `${CONFIG.PDM_TILES_URL}/${layer}/{z}/{x}/{y}.mvt` ],
+				minzoom: 7,
+				maxzoom: 14
+			};
+
+			layers.push({
+				id: id,
+				source: id,
+				type: "circle",
+				"source-layer": layer,
+				paint: Object.assign({ "circle-stroke-color": color }, circlePaint)
+			});
+
+			legend.push({ color, label: ds.name });
+		});
+
 		// Osmose
 		p.datasources
 		.filter(ds => ds.source === "osmose")
@@ -101,31 +126,6 @@ exports.getMapStyle = (p) => {
 				id: id,
 				source: id,
 				type: "circle",
-				paint: Object.assign({ "circle-stroke-color": color }, circlePaint)
-			});
-
-			legend.push({ color, label: ds.name });
-		});
-
-		// OSM
-		p.datasources
-		.filter(ds => ds.source === "osm")
-		.forEach((ds, dsid) => {
-			const id = `osm_${dsid}`;
-			const color = ds.color || "#2E7D32";
-			const layer = `public.project_${p.id.split("_").pop()}`;
-			sources[id] = {
-				type: "vector",
-				tiles: [ `${CONFIG.PDM_TILES_URL}/${layer}/{z}/{x}/{y}.mvt` ],
-				minzoom: 7,
-				maxzoom: 14
-			};
-
-			layers.push({
-				id: id,
-				source: id,
-				type: "circle",
-				"source-layer": layer,
 				paint: Object.assign({ "circle-stroke-color": color }, circlePaint)
 			});
 

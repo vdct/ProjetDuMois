18ba406872fd73013f7d139c135efa41a16e2509
PanierAvide <panieravide@riseup.net>
2021-01-22 10:35:09 +0100

    Fix undefined status in sendfile (see #150)

--- website/index.js
+++ website/index.js
@@ -419,7 +419,7 @@ app.get('/lib/:modname/:file', (req, res) => {
 	const fileName = `${req.params.modname}/${authorized[req.params.modname][req.params.file]}`;
 	res.sendFile(fileName, options, (err) => {
 		if (err) {
-			res.status(err.status).end();
+			res.status(err.status ? err.status : 500).end();
 		}
 	});
 });

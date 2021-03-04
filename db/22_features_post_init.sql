5f59b7415c6358befdcd5e4af585006a0c7d8066
PanierAvide <panieravide@riseup.net>
2021-03-04 20:06:30 +0100

    Fix count of features per boundary

--- db/22_features_post_init.sql
+++ db/22_features_post_init.sql
@@ -3,7 +3,7 @@ CREATE MATERIALIZED VIEW pdm_boundary_tiles AS
 SELECT
 	s.project, s.boundary, b.name, b.admin_level,
 	json_object(array_agg(s.day::text), array_agg(s.amount::text)) AS stats,
-	(array_agg(s.amount))[array_upper(array_agg(s.amount), 1)] AS nb,
+	MAX(s.amount) AS nb,
 	ST_PointOnSurface(b.geom)::GEOMETRY(Point, 3857) AS geom
 FROM pdm_feature_counts_per_boundary s
 JOIN pdm_boundary b ON s.boundary = b.osm_id

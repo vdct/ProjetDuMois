55bdbee0663deb0208c98248efcaa9f3a64a99ef
Adrien Pavie <panieravide@riseup.net>
2025-09-28 09:09:28 +0200

    Minor fixes on processing (#334)
    * Add soft end date for projects
    
    * Fix SQL + undefined end_date

--- db/23_changes_boundary.sql
+++ db/23_changes_boundary.sql
@@ -1,8 +1,8 @@
 -- Insert unknown changes from pdm_changes_tmp and associate them with enclosing boundaries
 WITH unknown AS (
 	SELECT pc.osmid AS osmid,
-	pc.version as version
-	b.osm_id AS boundary,
+	pc.version as version,
+	b.osm_id AS boundary
 	FROM pdm_changes_tmp pc
 	LEFT JOIN pdm_features_boundary fb ON fb.project=:project_id AND fb.osmid=pc.osmid AND fb.version=pc.version
 	JOIN pdm_boundary_subdivide b ON ST_Intersects(pc.geom, b.geom)
